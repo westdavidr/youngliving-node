576ae4ab491852f20ced0f94c4d7fddd440d2d35
David West <dwest@ambitenergy.com>
2017-06-19 12:42:35 -0500

    Minor tweaks.

--- example.js
+++ example.js
@@ -11,10 +11,11 @@ yl.use(login_options);
 var period = yl.get_period(); // or pass optional date parameter for a different period.
 var per_page = 200, page_number = 1;
 
-var handle_all_members = function(err, data) {
-    if(data.pagination.next) {
-      data.pagination.next(handle_all_members);
-    }
+var handle_all_members = function (err, data) {
+  
+  if (data.pagination.next) {
+    data.pagination.next(handle_all_members);
+  }
 }
 
 yl.all_members(period, per_page, page_number, handle_all_members);
